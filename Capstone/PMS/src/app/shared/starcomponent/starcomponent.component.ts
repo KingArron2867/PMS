5c9e2c6dfbb9f02f0f8f89d8e870f85d8421277d
Beans <pscbus_103@Beans-MBA.local>
2020-08-27 02:57:01 -0400

    created star component

---
+++ Capstone/PMS/src/app/shared/starcomponent/starcomponent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-starcomponent',
+  templateUrl: './starcomponent.component.html',
+  styleUrls: ['./starcomponent.component.css']
+})
+export class StarcomponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
