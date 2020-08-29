97b98ea90f089bcc1b7ef558419b086ba7b7007b
Beans <pscbus_103@Beans-MBA.local>
2020-08-29 12:50:15 -0400

    Added about-us.component with updated information

--- Capstone/PMS/src/app/app.module.ts
+++ Capstone/PMS/src/app/app.module.ts
@@ -8,19 +8,22 @@ import { WelcomeComponent } from './home/welcome.component';
 import { ProductModule } from './products/product.module';
 import { ContactComponent } from './contact/contact.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AboutUsComponent } from './about/about-us/about-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
     ContactComponent,
-    NavbarComponent
+    NavbarComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     RouterModule.forRoot([
       { path: 'welcome', component: WelcomeComponent },
+      { path: 'about', component: AboutUsComponent },
       {path: 'contact', component: ContactComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
